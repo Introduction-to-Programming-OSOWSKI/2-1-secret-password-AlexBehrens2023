4ff12a406ad4e39b08d98ed7bf68bd8e68e615fe
AlexBehrens2023 <816119@mystma.org>
2021-09-28 14:36:57 -0500

    2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #WRITE YOUR CODE IN THIS FILE
 def password(p):
-    if p == "Knights19":
+    if p.lower() == "Knights19":
         return "Accuss Granted"
     else:
         return "Accuss Denied"
